2ca075c00fdc21c4517b6b95edde0900771bb834
Mimioj101 <mimioj@gmail.com>
2021-01-05 10:16:33 -0500

    wrote hello world

--- hello-world/src/App.java
+++ hello-world/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        String helloWorld = "Hello, World!";
+        System.out.println(helloWorld);
     }
 }
